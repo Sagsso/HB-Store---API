c14f5bb67f9f7cad2105964183b4e871a1f5776e
sagsso <santiagogalvez17@gmail.com>
2020-08-02 15:43:06 -0500

    User CRUD done.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
+import 'reflect-metadata';
+
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import {createConnection,} from 'typeorm';
+
 import userRoutes from './routes/user.routes';
 
 const app = express();
+createConnection();
 
 //MIDDLEWARES
 app.use(cors());
