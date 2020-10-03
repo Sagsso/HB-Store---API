52e2c68e1aaafba6e3d156a389183998e6b8cf8f
sagsso <santiagogalvez17@gmail.com>
2020-10-03 00:43:41 -0500

    deleteProduct formated.Does not delete products with sales

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,6 +1,6 @@
-import {Request, Response} from 'express';
-import {getRepository} from 'typeorm';
-import {Product} from '../entities/Product';
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+import { Product } from '../entities/Product';
 
 export const getProducts = async (req: Request, res: Response): Promise<Response> => {
     const products = await getRepository(Product).find();
@@ -20,17 +20,27 @@ export const createProduct = async (req: Request, res: Response): Promise<Respon
 
 export const updateProduct = async (req: Request, res: Response): Promise<Response> => {
     const product = await getRepository(Product).findOne(req.params.id);
-    if(product){
+    if (product) {
         getRepository(Product).merge(product, req.body);
         const results = await getRepository(Product).save(product);
-        return  res.json(results);
+        return res.json(results);
     }
 
-    return res.status(404).json({ msg: 'Not product found'});
+    return res.status(404).json({ msg: 'Not product found' });
 }
 
 export const deleteProduct = async (req: Request, res: Response): Promise<Response> => {
-    const results = await getRepository(Product).delete(req.params.id);
-    return res.json(results);
+    try {
+        const product = await getRepository(Product).findOne(req.params.id, { relations: ["saleDetails"]});
+        if(product?.saleDetails.length) {
+            return res.json({msg: 'No es posible eliminar el producto porque registra ventas.',
+                error: false, deleted: false});
+        }
+        const deleted = await getRepository(Product).delete(req.params.id);
+        return res.json({ msg: deleted, error: false, deleted: true});
+    } catch (error) {
+        return res.json({ msg: error, error: true, deleted: false});
+
+    }
 }
 
